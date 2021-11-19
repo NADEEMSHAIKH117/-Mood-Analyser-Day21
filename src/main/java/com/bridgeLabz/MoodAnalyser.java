7c77e233fceb417da044f1e46069052a38606a91
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-11-19 18:56:31 +0530

    added

--- src/main/java/com/bridgeLabz/MoodAnalyser.java
+++ src/main/java/com/bridgeLabz/MoodAnalyser.java
@@ -1,7 +1,24 @@
 package com.bridgeLabz;
 												
 public class MoodAnalyser {
+	private String message;
 	
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+	
+	public MoodAnalyser() {
+		
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 	public String analyseMood(String message) {
 		if (message.toLowerCase().contains("sad")) {
 			return "Sad";
