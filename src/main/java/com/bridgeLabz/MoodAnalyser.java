e95cd9b3586d474c6ce5887fa7d7b403504e5a47
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-11-19 18:25:20 +0530

    added

--- src/main/java/com/bridgeLabz/MoodAnalyser.java
+++ src/main/java/com/bridgeLabz/MoodAnalyser.java
@@ -5,7 +5,10 @@ public class MoodAnalyser {
 	public String analyseMood(String message) {
 		if (message.toLowerCase().contains("sad")) {
 			return "Sad";
-		} else {
+		} else if(message.toLowerCase().contains("any")) {
+			return "Happy";
+			
+		}else {
 			return null;
 		}
 	}
--- src/test/java/com/bridgeLabzTest/MoodAnalyserTest.java
+++ src/test/java/com/bridgeLabzTest/MoodAnalyserTest.java
@@ -7,12 +7,19 @@ import org.junit.Test;
 import com.bridgeLabz.MoodAnalyser;
 
 public class MoodAnalyserTest {
-
+	MoodAnalyser moodanalyser = new MoodAnalyser();
 	@Test
-	public void test() {
+	public void giveMessage_IsProper_ShouldReturnSad () {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
 		String actualResult = moodAnalyser.analyseMood("I am in Sad Mood");
 		assertEquals("Sad", actualResult);
+	}
+	
+	@Test
+	public void giveMessage_IsProper_AndContain_Any_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String actualResult = moodAnalyser.analyseMood("I am in Any Mood");
+		assertEquals("Happy", actualResult);
 	}
 
 }
